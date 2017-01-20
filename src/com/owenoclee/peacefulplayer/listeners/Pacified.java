c1cc0e98a2f24025ed9a56ad1fbab1b33bd0ce73
Owen Oclee <git@gru.bz>
2017-01-20 23:37:34 +0000

    Fix pacified players being able to deal damage using projectiles v1.0.1

--- src/com/owenoclee/peacefulplayer/listeners/Pacified.java
+++ src/com/owenoclee/peacefulplayer/listeners/Pacified.java
@@ -4,6 +4,7 @@ import com.owenoclee.peacefulplayer.permissions.Permissions;
 import com.owenoclee.peacefulplayer.helper.Mobs;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -14,6 +15,11 @@ public class Pacified implements Listener {
     @EventHandler
     public void onAttack(EntityDamageByEntityEvent attackEvent) {
         Entity attacker = attackEvent.getDamager();
+        if(attacker instanceof Projectile) {
+            if(!(((Projectile)attacker).getShooter() instanceof Player))
+                return;
+            attacker = (Player)((Projectile)attackEvent.getDamager()).getShooter();
+        }
         Entity victim = attackEvent.getEntity();
         if(attacker instanceof Player && attacker.hasPermission(Permissions.PACIFIED.getPermission()) && (Mobs.isHostile(victim) || Mobs.isNeutral(victim)) ) {
             attackEvent.setDamage(0);
