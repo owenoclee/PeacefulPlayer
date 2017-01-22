03e313e7eb7d1744db0e4cf772e0241eaa06accf
Owen Oclee <git@gru.bz>
2017-01-22 13:20:13 +0000

    Fix pacified players dealing damage using splash/lingering potions

--- src/com/owenoclee/peacefulplayer/listeners/Pacified.java
+++ src/com/owenoclee/peacefulplayer/listeners/Pacified.java
@@ -7,7 +7,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.AreaEffectCloudApplyEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.PotionSplashEvent;
+import org.bukkit.projectiles.ProjectileSource;
 
 public class Pacified implements Listener {
 
@@ -24,6 +27,25 @@ public class Pacified implements Listener {
         if(attacker instanceof Player && attacker.hasPermission(Permissions.PACIFIED.getPermission()) && (Mobs.isHostile(victim) || Mobs.isNeutral(victim)) ) {
             attackEvent.setDamage(0);
             attackEvent.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onLingeringPotionEffect(AreaEffectCloudApplyEvent areaEffectEvent) {
+        ProjectileSource thrower = areaEffectEvent.getEntity().getSource();
+        if(thrower instanceof Player && ((Player)thrower).hasPermission(Permissions.PACIFIED.getPermission())) {
+            areaEffectEvent.getAffectedEntities().removeIf(mob -> Mobs.isHostile(mob) || Mobs.isNeutral(mob));
+        }
+    }
+
+    @EventHandler
+    public void onPotionEffect(PotionSplashEvent splashEvent) {
+        ProjectileSource thrower = splashEvent.getEntity().getShooter();
+        if(thrower instanceof Player && ((Player)thrower).hasPermission(Permissions.PACIFIED.getPermission())) {
+            splashEvent.getAffectedEntities().forEach(mob -> {
+                        if(Mobs.isHostile(mob) || Mobs.isNeutral(mob))
+                            splashEvent.setIntensity(mob, 0);
+                    });
         }
     }
 
